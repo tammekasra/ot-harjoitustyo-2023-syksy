cdd27fc69813187e7801fbfaa922209cfd65a425
Ranel Tammel <raneltam@ad.helsinki.fi>
2023-11-21 20:15:22 +0200

    Changes to be committed: 	new file:   .coveragerc 	modified:   poetry.lock 	modified:   pyproject.toml 	new file:   src/maksukortti.py 	new file:   src/tests/__init__.py 	new file:   src/tests/test_maksukortti.py 	modified:   tasks.py

---
+++ src/tests/test_maksukortti.py
@@ -0,0 +1,21 @@
+import unittest
+from src.maksukortti import Maksukortti  
+
+class TestMaksukortti(unittest.TestCase):
+    def test_kortin_saldo_alustetaan_oikein(self):
+        kortti = Maksukortti(1000)
+        self.assertEqual(kortti.saldo, 1000)
+
+    def test_syo_edullisesti_vahentaa_saldoa_oikein(self):
+        kortti = Maksukortti(1000)
+        kortti.syo_edullisesti()
+        self.assertEqual(kortti.saldo, 760)
+
+    def test_syo_maukkaasti_vahentaa_saldoa_oikein(self):
+        kortti = Maksukortti(1000)
+        kortti.syo_maukkaasti()
+        self.assertEqual(kortti.saldo, 600)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tasks.py
+++ tasks.py
@@ -27,3 +27,15 @@ def coverage_report(ctx):
     ctx.run("coverage html", pty=True)
     if platform != "win32":
         call(("xdg-open", "htmlcov/index.html"))
+@task
+def start(ctx):
+    ctx.run("poetry run python src/main.py", pty=True)
+@task
+def test(ctx):
+    ctx.run("poetry run pytest src")
+
+@task
+def coverage_report(ctx):
+    ctx.run("poetry run coverage run --branch -m pytest src")
+    ctx.run("poetry run coverage report -m")
+    ctx.run("poetry run coverage html")
