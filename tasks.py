d20e34bcf4ade77d4390b64de080429753f56ffe
tammekasra <54755302+tammekasra@users.noreply.github.com>
2023-12-05 15:22:57 +0200

    Fixes

--- tasks.py
+++ tasks.py
@@ -4,44 +4,32 @@ from sys import platform
 
 
 @task
-def foo(ctx):
-    print("bar")
-
-@task
 def start(ctx):
-    ctx.run("python3 src/index.py", pty=True)
+    ctx.run("python3 src/main.py", pty=True)
 
 
-@task
-def lorem_ipsum(ctx):
-    print("Lorem ipsum")
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=True)
 
-@task(coverage)
-def coverage_report(ctx):
-    ctx.run("coverage html", pty=True)
 
 @task(coverage)
 def coverage_report(ctx):
     ctx.run("coverage html", pty=True)
     if platform != "win32":
         call(("xdg-open", "htmlcov/index.html"))
-@task
-def start(ctx):
-    ctx.run("poetry run python src/main.py", pty=True)
+
+
 @task
 def test(ctx):
     ctx.run("poetry run pytest src")
 
+
 @task
-def coverage_report(ctx):
+def coverage_report_poetry(ctx):
     ctx.run("poetry run coverage run --branch -m pytest src")
     ctx.run("poetry run coverage report -m")
     ctx.run("poetry run coverage html")
-    
-    
 
 
 @task
